720128c96d3924f233b854a87e7e843fa2adfc90
pauleena phan <pauleena2002@gmail.com>
2024-06-19 21:30:41 -0700

    connected homepage

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const homeRouter = require("./routes/home");
 
 const mongoose = require("mongoose");
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use("/home", homeRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.redirect("/home")
+router.get('/', function(req, res) {
+  res.redirect("/home");
 });
 
 module.exports = router;
